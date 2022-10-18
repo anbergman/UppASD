81739cd545ca19ee38e826cc82e0361fb244639b
Anders Bergman <anders.bergman@physics.uu.se>
2022-10-18 13:29:17 +0200

    Using Ninja as cmake generator.

--- python/setup.py
+++ python/setup.py
@@ -32,6 +32,7 @@ class cmake_build_ext(build_ext):
             cfg = 'Debug' if os.environ.get('DISPTOOLS_DEBUG','OFF') == 'ON' else 'Release'
 
             cmake_args = [
+                '-G Ninja,
                 '-DCMAKE_BUILD_TYPE=%s' % cfg,
                 # Ask CMake to place the resulting library in the directory
                 # containing the extension
