c5bbeb6f5bc1074f4c45db449c6f696afcad2379
Anders Bergman <anders.bergman@physics.uu.se>
2023-07-24 08:22:11 +0200

    Reinstated .github/workflows/wheels_windows.yml

--- uppasd/__init__.py
+++ uppasd/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import print_function, absolute_import, division
 import _uppasd
 #import uppasd
-import f90wrap.runtime
-import logging
-from . import pyasd
-from . import simulationdata
-from . import momentdata
-from . import inputdatatype
-from . import inputdata
-from . import inputhandler_ext
-from . import inputhandler
+#import f90wrap.runtime
+#import logging
+#from . import pyasd
+#from . import simulationdata
+#from . import momentdata
+#from . import inputdatatype
+#from . import inputdata
+#from . import inputhandler_ext
+#from . import inputhandler
